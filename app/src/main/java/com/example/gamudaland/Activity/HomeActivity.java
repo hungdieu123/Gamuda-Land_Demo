91c4b1a6414bf4e51f5d752325314db76d7b2efe
Hungkaka <hungdieu182@gmail.com>
2019-11-11 13:28:02 +0700

    AsssgDGDD_part1.3

--- app/src/main/java/com/example/gamudaland/Activity/HomeActivity.java
+++ app/src/main/java/com/example/gamudaland/Activity/HomeActivity.java
@@ -44,6 +44,7 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        //
     }
 
     @Override
